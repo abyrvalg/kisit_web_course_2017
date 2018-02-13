cb545d617528a9c9fa3ace4c24aad1a73e6bbe4e
Sergey <sergey@gypost.com>
2018-02-13 21:48:48 +0200

    fs examples

--- Lab3/demo/myapp/routes/index.js
+++ Lab3/demo/myapp/routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var forms = require('./../forms');
+var fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -12,7 +13,31 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/admin', function(req, res){
+	res.render('adminLogin');
+})
+
+router.get('/register', function(req, res){
+	res.render('register');
+})
+
+router.post('/registerSubmit', function(req, res){
 	
+	fs.readFile('./data/users.json', function(err, data){
+
+		if(err){
+			console.log(err)
+		}
+		var users = JSON.parse(data);
+		users.push({
+			login : req.param('login'),
+			pwd : req.param('password')
+		});
+		
+		fs.writeFile('./data/users.json', JSON.stringify(users), function(err, data){
+			res.send('write')
+		})
+		
+	})
 })
 
 router.get('/fillForm', function(req, res){
